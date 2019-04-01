38c03a4af6ef3cb290e7ba845c82b9b4090a2c6a
Felix Stiehler <felix.stiehler87@gmail.com>
2019-04-01 14:52:24 +0200

    set zip_safe=false to inspect code in debugger

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
    description='Schema and API for a relational db that encodes gene models in an explicit, structured, and robust fashion.',
    author='Alisandra Denton',
    packages=['geenuff', 'geenuff.base', 'geenuff.applications', 'geenuff.tests'],  #same as name
+   zip_safe=False,
 )
 
