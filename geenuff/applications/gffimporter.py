02594a640abe6a624d3e48ad3e638c2831a06ac9
Felix Stiehler <felix.stiehler87@gmail.com>
2019-04-29 09:25:54 +0200

    moved MerCounter to be an inner class of CoordinateHandler

--- geenuff/applications/gffimporter.py
+++ geenuff/applications/gffimporter.py
@@ -1,19 +1,19 @@
-from dustdas import gffhelper, fastahelper
 import intervaltree
+import copy
+import logging
+import hashlib
+import itertools
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from dustdas import gffhelper, fastahelper
 from .. import orm
 from .. import types
 from .. import handlers
 from ..base.transcript_interp import TranscriptInterpBase, EukTranscriptStatus
-from ..base.mers import MerCounter
 from .. import helpers
 
-import copy
-import logging
-import hashlib
-
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 
 
 class IntervalCountError(Exception):
@@ -281,6 +281,39 @@ class GenomeHandler(handlers.GenomeHandlerBase):
 
 
 class CoordinateHandler(handlers.CoordinateHandlerBase):
+    class MerCounter(object):
+        amb = 'ambiguous_mers'
+
+        def __init__(self, k):
+            self.k = k
+            self.counts = {}
+            # calculate all possible mers of this length, and set counter to 0
+            for mer in itertools.product('ATCG', repeat=k):
+                mer = ''.join(mer)
+                self.counts[mer] = 0
+            self.counts[MerCounter.amb] = 0
+            # most recent base pairs of up to length k
+            self.sliding_mer = []
+
+        def export(self):
+            out = copy.deepcopy(self.counts)
+            for key in self.counts:
+                if key != MerCounter.amb:
+                    # collapse to cannonical kmers
+                    rc_key = helpers.reverse_complement(key)
+                    if key != min(key, rc_key):
+                        out[rc_key] += out[key]
+                        out.pop(key)
+            return out
+
+        def add_sequence(self, sequence):
+            for i in range(len(sequence) -  self.k + 1):
+                mer = sequence[i:i+self.k]
+                try:
+                    self.counts[mer] += 1
+                except KeyError:
+                    self.counts[MerCounter.amb] += 1
+
     def __init__(self, data=None, controller=None):
         super().__init__()
         self.add_data(data)
