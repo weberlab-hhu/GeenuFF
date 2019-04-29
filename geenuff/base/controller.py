c2b4dda373ecf3ad3c13e871dd308b4bca0af1da
Felix Stiehler <felix.stiehler87@gmail.com>
2019-04-29 17:07:54 +0200

    minor updates to Controller class

--- geenuff/base/controller.py
+++ geenuff/base/controller.py
@@ -7,15 +7,13 @@ class Controller(object):
     """Base class for objects the manipulate a GeenuFF database."""
 
     def __init__(self, database_path, err_path='/dev/null'):
-        self.database_path = Controller.full_db_path(database_path)
+        self.database_path = database_path
         self.err_path = err_path
-        self.engine = None
-        self.session = None
         self.engine, self.session = Controller.mk_session(self.database_path)
 
     @staticmethod
     def mk_session(database_path):
-        engine = create_engine(database_path, echo=False)
+        engine = create_engine(Controller.full_db_path(database_path), echo=False)
         orm.Base.metadata.create_all(engine)
         session = sessionmaker(bind=engine)()
         return engine, session
