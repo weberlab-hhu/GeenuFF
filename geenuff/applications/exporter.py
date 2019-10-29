689c03936481a3612fddd0099f01e5dd27263244
Felix Stiehler <felix.stiehler87@gmail.com>
2019-10-29 15:30:34 +0100

    quick fix for slow error feature query

--- geenuff/applications/exporter.py
+++ geenuff/applications/exporter.py
@@ -97,13 +97,14 @@ class GeenuffExportController(object):
             error_features_query = (self.session.query(Feature, Coordinate.id, Coordinate.length,
                                                        Coordinate.genome_id)
                                        .join(Coordinate, Feature.coordinate_id == Coordinate.id)
-                                       .join(Genome, Genome.id == Coordinate.genome_id)
-                                       .filter(Feature.type.in_(error_type_values)))
+                                       .join(Genome, Genome.id == Coordinate.genome_id))
+                                       # .filter(Feature.type.in_(error_type_values)))
             if genomes:
                 error_features_query = error_features_query.filter(Genome.species.in_(genomes))
             elif exclude:
                 error_features_query = error_features_query.filter(Genome.species.notin_(genomes))
             error_features = error_features_query.all()
+            error_features = [r for r in error_features if r[0].type.value in error_type_values]
             print('Query for error features took {:.2f}s'.format(time.time() - regular_time))
 
             all_coords_with_features = regular_features + error_features
