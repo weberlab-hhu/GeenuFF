062272032f990134fe3e0e1312e1105c6e1614bb
Felix Stiehler <felix.stiehler87@gmail.com>
2019-11-28 11:11:30 +0100

    cross joins started

--- geenuff/applications/exporter.py
+++ geenuff/applications/exporter.py
@@ -56,33 +56,22 @@ class GeenuffExportController(object):
                         .join(Feature, asso_tp_2_f.c.feature_id == Feature.id)
                         .join(Coordinate, Feature.coordinate_id == Coordinate.id)
                         .join(Genome, Genome.id == Coordinate.genome_id))
-        else:
-            query = (self.session.query(Feature, Coordinate.id, Coordinate.length, Coordinate.genome_id)
-                        .join(Coordinate, Feature.coordinate_id == Coordinate.id)
-                        .join(asso_tp_2_f, asso_tp_2_f.c.feature_id == Feature.id)
-                        .join(TranscriptPiece, asso_tp_2_f.c.transcript_piece_id == TranscriptPiece.id)
-                        .join(Transcript, TranscriptPiece.transcript_id == Transcript.id)
-                        .join(SuperLocus, Transcript.super_locus_id == SuperLocus.id)
-                        .join(Genome, Genome.id == Coordinate.genome_id)
-                        .filter(Transcript.longest == True))
+            # filter to common Transcript and Super Locus type in both cases
+            query = (query
+                        .filter(Transcript.type == types.TranscriptLevel.mRNA)
+                        .filter(SuperLocus.type == types.SuperLocusAll.gene))
 
-        # filter to common Transcript and Super Locus type in both cases
-        query = (query
-                    .filter(Transcript.type == types.TranscriptLevel.mRNA)
-                    .filter(SuperLocus.type == types.SuperLocusAll.gene))
-
-        if genomes:
-            print('Selecting the following genomes: {}'.format(genomes), file=sys.stderr)
-            query = query.filter(Genome.species.in_(genomes))
-        else:
-            if exclude:
-                print('Selecting all genomes from {} except: {}'.format(self.db_path_in, exclude),
-                      file=sys.stderr)
-                query = query.filter(Genome.species.notin_(exclude))
+            if genomes:
+                print('Selecting the following genomes: {}'.format(genomes), file=sys.stderr)
+                query = query.filter(Genome.species.in_(genomes))
             else:
-                print('Selecting all genomes from {}'.format(self.db_path_in), file=sys.stderr)
+                if exclude:
+                    print('Selecting all genomes from {} except: {}'.format(self.db_path_in, exclude),
+                          file=sys.stderr)
+                    query = query.filter(Genome.species.notin_(exclude))
+                else:
+                    print('Selecting all genomes from {}'.format(self.db_path_in), file=sys.stderr)
 
-        if return_super_loci:
             query = (query
                         .order_by(Genome.species)
                         .order_by(Coordinate.length.desc())
@@ -90,17 +79,24 @@ class GeenuffExportController(object):
                         .order_by(Feature.start))
             return query.all()
         else:
-            print('Querying all relevant features...')
+            query = '''SELECT feature.id AS feature_id, feature.given_name AS feature_given_name, feature.type AS feature_type, feature.start AS feature_start, feature.start_is_biological_start AS feature_start_is_biological_start, feature."end" AS feature_end, feature.end_is_biological_end AS feature_end_is_biological_end, feature.is_plus_strand AS feature_is_plus_strand, feature.score AS feature_score, feature.source AS feature_source, feature.phase AS feature_phase, feature.coordinate_id AS feature_coordinate_id, coordinate.id AS coordinate_id, coordinate.length AS coordinate_length, coordinate.genome_id AS coordinate_genome_id
+FROM genome
+CROSS JOIN coordinate ON coordinate.genome_id = genome.id
+CROSS JOIN feature ON feature.coordinate_id = coordinate.id
+CROSS JOIN association_transcript_piece_to_feature ON association_transcript_piece_to_feature.feature_id = feature.id
+CROSS JOIN transcript_piece ON association_transcript_piece_to_feature.transcript_piece_id = transcript_piece.id
+CROSS JOIN transcript ON transcript_piece.transcript_id = transcript.id
+CROSS JOIN super_locus ON transcript.super_locus_id = super_locus.id
+WHERE transcript.longest = 1 AND genome.species IN ('caenorhabditis_elegans') and super_locus.type = 'gene' and transcript.type = 'mRNA'
+ORDER BY genome.species, coordinate.length DESC;'''
             start = time.time()
+            import pudb; pudb.set_trace()
+            res = self.engine.execute(query)
             # all_coords_with_features = (self.session.query(Feature, Coordinate.id, Coordinate.length,
                                            # Coordinate.genome_id)
                         # .join(Coordinate, Feature.coordinate_id == Coordinate.id)
                         # .limit(10)
                         # .all())
-            all_coords_with_features = (query
-                                           .order_by(Genome.species)
-                                           .order_by(Coordinate.length.desc())
-                                           .all())
             print('Query took {:.2f}s'.format(time.time() - start))
 
             # reorganizing rows into genome centric dict
