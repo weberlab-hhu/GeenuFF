1a42276821a74d21455ec5345f055be2b8a3c418
alisandra <alisandra.denton@hhu.de>
2020-07-23 15:49:31 +0200

    include testdata in install

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
    name='geenuff',
    version='0.1',
    description='Schema and API for a relational db that encodes gene models in an explicit, structured, and robust fashion.',
-   author='Alisandra Denton',
+   author='Alisandra Denton, Felix Stiehler',
    packages=['geenuff', 'geenuff.base', 'geenuff.applications', 'geenuff.tests'],  #same as name
+   package_data = {'geenuff': ['testdata/*.fa', 'testdata/*.gff3', 'testdata/*.gff']},
    install_requires=["intervaltree>=3.0.2", "SQLAlchemy>=1.3.12", "numpy>=1.18.1", "dustdas @ git+https://github.com/janinamass/dustdas@master", "pytest>=5.3.4"],
    zip_safe=False,
 )
