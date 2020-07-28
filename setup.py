16a6a22b15a27eb6e4b0a509b0d56ca884dc5e74
alisandra <alisandra.denton@hhu.de>
2020-07-28 15:16:16 +0200

    incl. exporters

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
    version='0.1',
    description='Schema and API for a relational db that encodes gene models in an explicit, structured, and robust fashion.',
    author='Alisandra Denton, Felix Stiehler',
-   packages=['geenuff', 'geenuff.base', 'geenuff.applications', 'geenuff.tests'],  #same as name
+   packages=['geenuff', 'geenuff.base', 'geenuff.applications', 'geenuff.applications.exporters', 'geenuff.tests'],  #same as name
    package_data = {'geenuff': ['testdata/*.fa', 'testdata/*.gff3', 'testdata/*.gff']},
    install_requires=["intervaltree>=3.0.2", "SQLAlchemy>=1.3.12", "numpy>=1.18.1", "dustdas @ git+https://github.com/janinamass/dustdas@master", "pytest>=5.3.4"],
    scripts=['import2geenuff.py'],
