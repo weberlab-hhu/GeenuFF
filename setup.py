ff7723846c49e12732f309c11efbd7cb4a6e3861
alisandra <alisandra.denton@hhu.de>
2024-03-23 16:24:05 -0400

    dustdas version

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ except ImportError:
 
 setup(
    name='geenuff',
-   version='0.3.0',
+   version='0.3.1',
    description='Schema and API for a relational db that encodes gene models in an explicit, structured, and robust fashion.',
-   author='Alisandra Denton, Felix Stiehler',
+   author='Alisandra Denton, Felix Holst',
    packages=['geenuff', 'geenuff.base', 'geenuff.applications', 'geenuff.applications.exporters', 'geenuff.tests'],  #same as name
    package_data={'geenuff': ['testdata/*.fa', 'testdata/*.gff3', 'testdata/*.gff']},
-   install_requires=["intervaltree>=3.0.2", "SQLAlchemy>=1.3.12", "numpy>=1.18.1", "dustdas @ git+https://github.com/janinamass/dustdas@master", "pytest>=5.3.4"],
+   install_requires=["intervaltree>=3.0.2", "SQLAlchemy>=1.3.12", "numpy>=1.18.1", "dustdas @ git+https://github.com/gglyptodon/dustdas@v0.2", "pytest>=5.3.4"],
    scripts=['import2geenuff.py'],
    zip_safe=False,
 )
